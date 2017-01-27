f78ed6cc115ab56a0df2483bbf58ba5fa61b29d7
Zontzip <zontzor@gmail.com>
2017-01-27 20:52:34 +0000

    Rename shapes dir and minor bug fixes

--- week_1/area_shapes/main.c
+++ week_1/area_shapes/main.c
@@ -1,3 +1,10 @@
+/**
+ * Program allowing for computation of area of some shapes.
+ *  
+ * Compile using following command:
+ * gcc main.c area_rect.c area_cir.c area_tri.c -o bin/main
+ * 
+**/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,6 +13,11 @@
 #include "area_tri.h"
 
 int main(int argc, char* argv[]) {
+  if (argc < 3 || argc > 4) {
+    printf("Wrong number of arguments\n");
+    return 0;
+  }
+  
   if (strcmp(argv[1], "rect") == 0) {
     if (argc == 4) {
       int width = atoi(argv[2]);
