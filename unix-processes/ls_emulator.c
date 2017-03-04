c57f9173ca78abd65a9571b71eaa1e97c306a351
Zontzip <zontzor@gmail.com>
2017-03-04 20:25:35 +0000

    Add ls simulator

--- unix-processes/ls_emulator.c
+++ unix-processes/ls_emulator.c
@@ -1,53 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-
-int main( int argc, char *argv[] )  {
-
-	int link[2];
-	pid_t pid;
-	char foo[4096];
-
-	if (pipe(link)==-1) {
-		exit(27);
-	}
-
-	if ((pid = fork()) == -1) {
-		exit(27);
-	}
-
-	if(pid == 0) {
-
-		dup2 (link[1], STDOUT_FILENO);
-		close(link[0]);
-		close(link[1]);
-		execl("/bin/ls", "ls", "-1", (char *)0);
-		exit(27);
-
-	} else {
-
-		close(link[1]);
-
-		int nbytes = read(link[0], foo, sizeof(foo));
-
-		if ( argc == 2 ) {
-			printf("Finding... %s\n",argv[1]);
-
-			for(int i = 0; i < sizeof(foo); i++)  {
-				if(strcmp(argv[1], &foo[i]) == 0 ) {
-					printf("Found file: %c\n", foo[i]);
-				}
-			}
-			wait(NULL);
-
-		} else {
-
-			printf("Output: (%.*s)\n", nbytes, foo);
-			wait(NULL);
-		}
-
-	}
-	return 0;
+int main() {
+  char *argv[]={"ls", "-l", (char *)0};
+  execv("/bin/ls", argv);
+  return 0;
 }
