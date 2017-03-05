787058e87c74e9d14596c5dce2def8f02eff65f0
Zontzip <zontzor@gmail.com>
2017-03-05 12:39:41 +0000

    Update daemon

--- daemon-processes/basic_daemon.c
+++ daemon-processes/basic_daemon.c
@@ -2,9 +2,11 @@
   Orphan process
   The child process is adopted by init process, when parent process dies
 */
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/stat.h>
 
 int main() {
   // Create a child process
@@ -13,16 +15,44 @@ int main() {
   // If PID > 0, then it is the parent
   if (pid > 0) {
     printf("Parent process\n");
-    sleep(10);
+    // sleep(5);
     exit(EXIT_SUCCESS);
-  } else {
+  } else if (pid == 0) {
     // Step 1: Create the orphan process
     printf("Child process\n");
 
-    // Orphan logic goes here!
-    // The process is kept running in an infinite loop.
-    // When the parent finishes after 10 seconds,
-    // the getppid() will return 1 as the parent (init process)
+    /*  
+      Step 2: Elevate the orphan process to session leader, to lose
+      controlling TTY (terminal).   
+      This command runs the process in a new session
+    */
+    if (setsid() < 0) {
+      exit(EXIT_FAILURE);
+    };
+    
+    /*
+      Step 3: Call umask() to set the file mode creation mask to 0.
+      This will allow the daemon to rear and write file with the permissions/
+      access required
+    */
+    umask(0);
+    
+    /*
+      Step 4: Change the current working dit to root.
+      This will eliminate any issue of running on a mounted drive,
+      that potentially could be removed, etc...
+    */
+    if (chdir("/") < 0) {
+      exit(EXIT_FAILURE);
+    };
+    
+    /*
+      Step 5: Close all open file descriptors
+    */
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x >= 0; x--) {
+      close(x);
+    }
 
     while(1) {
       sleep(1);
