e109c5da506252b9df963e321c6b607227790923
alex <zontzor@gmail.com>
2017-04-07 14:27:24 +0100

    Update server and client with basic functionality

--- file-transfer/client/src/main.c
+++ file-transfer/client/src/main.c
@@ -1,8 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 
 int main() {
-  
+  int SID;
+  struct sockaddr_in server;
+  char clientMessage[500];
+  char serverMessage[500];
+
+  SID = socket(AF_INET, SOCK_STREAM, 0);
+  if (SID == -1) {
+    printf("Error creating socket\n");
+  } else {
+    printf("Socket created\n");
+  }
+
+  server.sin_port = htons(8081);
+  server.sin_addr.s_addr = inet_addr("127.0.0.1");
+  server.sin_family = AF_INET;
+
+  if(connect(SID, (struct sockaddr*)&server, sizeof(server)) < 0) {
+    printf("Connect failed. Error\n");
+    return 1;
+  }
+
+  printf("Connected to server\n");
+
+  while(1) {
+    printf("Enter a message: ");
+    scanf("%s", clientMessage);
+
+    if (send(SID, clientMessage, strlen(clientMessage), 0) < 0) {
+      printf("Send failed\n");
+      return 1;
+    }
+
+    if (recv(SID, serverMessage, 500, 0) < 0) {
+      printf("IO error\n");
+      break;
+    }
+
+    printf("Server sent: ");
+    printf("%s", serverMessage);
+
+    close(SID);
+  }
+
+  return 0;
 }
--- file-transfer/server/src/main.c
+++ file-transfer/server/src/main.c
@@ -1,8 +1,65 @@
+/*
+  Pass client accpetted socket
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
 #include <sys/socket.h>
 
 int main() {
-  
+  int s;
+  int cs;
+  int connSize;
+  int READSIZE;
+
+  struct sockaddr_in server, client;
+  char message[500];
+
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s == -1) {
+    printf("Could not create socket\n");
+  } else {
+    printf("Socket successfully created\n");
+  }
+
+  server.sin_port = htons(8081);
+  server.sin_family = AF_INET;
+  server.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    perror("Bind issue\n");
+    return 1;
+  } else {
+    printf("Bind complete\n");
+  }
+
+  listen(s, 3);
+  printf("Waiting for incoming connection from client\n");
+  connSize = sizeof(struct sockaddr_in);
+
+  while(1) {
+    cs = accept(s, (struct sockaddr *)&client, (socklen_t*)&connSize);
+    if (cs < 0) {
+      perror("Can't establish connection\n");
+      return 1;
+    } else {
+      printf("Connection from client accepted\n");
+    }
+
+    memset(message, 0, 500);
+    READSIZE = recv(cs, message, 2000, 0);
+    printf("Client said: %s\n", message);
+    write(cs, "Recieved", strlen("Recieved"));
+
+    if (READSIZE == 0) {
+      puts("Client disconnected\n");
+      fflush(stdout);
+    } else if(READSIZE == -1) {
+      perror("Read error\n");
+    }
+  }
+
+  return 0;
 }
