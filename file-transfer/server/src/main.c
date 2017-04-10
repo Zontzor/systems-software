e2bac595fb700e9c381bdcf100493a207271fd65
Zontzip <zontzor@gmail.com>
2017-04-10 11:02:58 +0100

    Add multithreaded chat server and client

--- file-transfer/server/src/main.c
+++ file-transfer/server/src/main.c
@@ -1,65 +1,97 @@
 /*
-  Pass client accpetted socket
- */
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-
-int main() {
-  int s;
-  int cs;
-  int connSize;
-  int READSIZE;
-
+  Multi-threaded socket server
+**/
+#include<stdio.h>
+#include<string.h>    
+#include<stdlib.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<pthread.h>
+ 
+void *connection_handler(void *);
+ 
+int main(int argc , char *argv[]) {
+  int socket_desc, client_sock, c , *new_sock;
   struct sockaddr_in server, client;
-  char message[500];
-
-  s = socket(AF_INET, SOCK_STREAM, 0);
-  if (s == -1) {
-    printf("Could not create socket\n");
-  } else {
-    printf("Socket successfully created\n");
+   
+  // Create socket
+  socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_desc == -1) {
+    printf("Could not create socket");
   }
-
-  server.sin_port = htons(8081);
+  puts("Socket created");
+   
+  // Prepare the sockaddr_in structure
   server.sin_family = AF_INET;
   server.sin_addr.s_addr = INADDR_ANY;
-
-  if (bind(s, (struct sockaddr *)&server, sizeof(server)) < 0) {
-    perror("Bind issue\n");
+  server.sin_port = htons(8888);
+   
+  // Bind
+  if(bind(socket_desc,(struct sockaddr *) &server, sizeof(server)) < 0) {
+    perror("bind failed. Error");
     return 1;
-  } else {
-    printf("Bind complete\n");
   }
-
-  listen(s, 3);
-  printf("Waiting for incoming connection from client\n");
-  connSize = sizeof(struct sockaddr_in);
-
-  while(1) {
-    cs = accept(s, (struct sockaddr *)&client, (socklen_t*)&connSize);
-    if (cs < 0) {
-      perror("Can't establish connection\n");
+  puts("Bind completed");
+   
+  // Listen
+  listen(socket_desc , 3);
+   
+  // Accept incoming connections
+  puts("Waiting for incoming connections...");
+  c = sizeof(struct sockaddr_in);
+  while((client_sock = accept(socket_desc, (struct sockaddr *) &client, (socklen_t*)&c))) {
+    puts("Connection accepted");
+     
+    pthread_t sniffer_thread;
+    new_sock = malloc(1);
+    *new_sock = client_sock;
+     
+    if(pthread_create(&sniffer_thread, NULL, connection_handler, (void*) new_sock) < 0)
+    {
+      perror("could not create thread");
       return 1;
-    } else {
-      printf("Connection from client accepted\n");
     }
-
-    memset(message, 0, 500);
-    READSIZE = recv(cs, message, 2000, 0);
-    printf("Client said: %s\n", message);
-    write(cs, "Recieved", strlen("Recieved"));
-
-    if (READSIZE == 0) {
-      puts("Client disconnected\n");
-      fflush(stdout);
-    } else if(READSIZE == -1) {
-      perror("Read error\n");
-    }
+     
+    // Join the thread 
+    //pthread_join(sniffer_thread, NULL);
+    puts("Handler assigned");
   }
-
+   
+  if (client_sock < 0) {
+    perror("accept failed");
+    return 1;
+  }
+   
+  return 0;
+}
+ 
+/*
+  Handle client connection
+**/
+void *connection_handler(void *socket_desc) {
+  // Get the socket descriptor
+  int sock = *(int*)socket_desc;
+  int read_size;
+  char *message, client_message[2000];
+  
+  // Receive message from client
+  while((read_size = recv(sock, client_message, 2000, 0)) > 0) {
+    // Send message back to client
+    write(sock, client_message, strlen(client_message) + 1);
+    memset(client_message, 0, sizeof(client_message));
+  }
+   
+  if(read_size == 0) {
+    puts("Client disconnected");
+    fflush(stdout);
+  }
+  else if(read_size == -1) {
+    perror("recv failed");
+  }
+       
+  // Free socket pointer
+  free(socket_desc);
+   
   return 0;
 }
