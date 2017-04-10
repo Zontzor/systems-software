b628c631c911bee97bf9d8ed63eab85dd41b16aa
Zontzip <zontzor@gmail.com>
2017-04-10 11:03:37 +0100

    Add chat client

--- file-transfer/client/src/main.c
+++ file-transfer/client/src/main.c
@@ -1,53 +1,58 @@
+/*
+  Socket client
+**/
+ 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h> 
+#include <sys/socket.h> 
 #include <arpa/inet.h>
-#include <sys/socket.h>
-
-int main() {
-  int SID;
+ 
+int main(int argc, char *argv[]) {
+  int sock;
   struct sockaddr_in server;
-  char clientMessage[500];
-  char serverMessage[500];
-
-  SID = socket(AF_INET, SOCK_STREAM, 0);
-  if (SID == -1) {
-    printf("Error creating socket\n");
-  } else {
-    printf("Socket created\n");
+  char message[1000], server_reply[2000];
+   
+  // Create socket
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1) {
+    printf("Could not create socket");
   }
-
-  server.sin_port = htons(8081);
+  puts("Socket created");
+   
   server.sin_addr.s_addr = inet_addr("127.0.0.1");
   server.sin_family = AF_INET;
+  server.sin_port = htons(8888);
 
-  if(connect(SID, (struct sockaddr*)&server, sizeof(server)) < 0) {
-    printf("Connect failed. Error\n");
+  // Connect to remote server
+  if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    perror("connect failed. Error");
     return 1;
   }
-
-  printf("Connected to server\n");
-
+   
+  puts("Connected\n");
+   
+  // Keep communicating with server
   while(1) {
-    printf("Enter a message: ");
-    scanf("%s", clientMessage);
-
-    if (send(SID, clientMessage, strlen(clientMessage), 0) < 0) {
-      printf("Send failed\n");
+    printf("Enter message: ");
+    scanf("%s", message);
+     
+    // Send some data
+    if(send(sock, message, strlen(message), 0) < 0) {
+      puts("Send failed");
       return 1;
     }
-
-    if (recv(SID, serverMessage, 500, 0) < 0) {
-      printf("IO error\n");
+     
+    // Receive a reply from the server
+    if(recv(sock, server_reply, 2000 , 0) < 0) {
+      puts("recv failed");
       break;
     }
-
-    printf("Server sent: ");
-    printf("%s", serverMessage);
-
-    close(SID);
+     
+    puts("Server reply: ");
+    puts(server_reply);
   }
-
+   
+  close(sock);
   return 0;
 }
